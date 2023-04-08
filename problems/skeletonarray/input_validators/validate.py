a0df1d694c60de9d02b35826fe7b1ffab279063a
lowekoz <lowe.k172@yahoo.com>
2023-04-09 00:11:30 +0200

    [skeletonarray] fix validate and statement

--- problems/skeletonarray/input_validators/validate.py
+++ problems/skeletonarray/input_validators/validate.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
-n = int(input())
+
+import sys
+
+lines = sys.stdin.readlines()
+correct_inp = []
+
+#windows newline
+for i in range(len(lines)):
+    lines[i] = lines[i].replace('\r',"")
+
+n = int(lines[0])
 assert(n >= 1)
 assert(n <= 100000)
-a = list(map(int,input().split()))
+
+correct_inp.append("%d\n" % n)
+
+a = list(lines[1].split())
 for el in a:
-    assert(abs(el) <= 1000)
+    assert(abs(int(el)) <= 1000)
+
+correct_inp.append(" ".join(a) + '\n')
+assert "".join(correct_inp) == "".join(lines)
+
 exit(42)
