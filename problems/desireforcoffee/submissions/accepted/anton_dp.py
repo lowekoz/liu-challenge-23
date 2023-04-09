eeb4540c7c86c5a8c0034094180ec89050643378
Anton <antel460@student.liu.se>
2023-04-09 16:34:18 +0200

    Desire for coffee: replaced cache with defaultdict

--- problems/desireforcoffee/submissions/accepted/anton_dp.py
+++ problems/desireforcoffee/submissions/accepted/anton_dp.py
@@ -1,17 +1,35 @@
 import sys
-from functools import cache
+from collections import defaultdict
+
+def def_value():
+    return -1
+
+dp = defaultdict(def_value)
 
 sys.setrecursionlimit(15000)
-@cache
 def max_stack(capacity, current_turtle):
     global turtles
+    global dp
+    
     if current_turtle == len(turtles):
         return 0
     dist1 = 0
+
     if capacity - turtles[current_turtle][1] >= 0:  
         local_capacity = min(capacity-turtles[current_turtle][1], turtles[current_turtle][0])
-        dist1 = turtles[current_turtle][2] + max_stack(local_capacity, current_turtle+1)
-    dist2 = max_stack(capacity, current_turtle+1)
+        if dp[(local_capacity, current_turtle+1)] != -1:
+            sub_stack = dp[(local_capacity, current_turtle+1)]
+        else:
+            sub_stack = max_stack(local_capacity, current_turtle+1)
+        dist1 = turtles[current_turtle][2] + sub_stack
+
+    if dp[(capacity, current_turtle+1)] != -1:
+        sub_stack = dp[(capacity, current_turtle+1)]
+    else:
+        sub_stack = max_stack(capacity, current_turtle+1)
+    dist2 = sub_stack
+
+    dp[(capacity, current_turtle)] = max(dist1, dist2)
     return max(dist1, dist2)
 
 
