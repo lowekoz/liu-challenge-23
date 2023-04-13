16f51b014ecd99509988c18da74e17e7831ede0c
Herman Lauenstein <herman.lauenstein@gmail.com>
2023-04-13 10:09:35 +0200

    fix and run generator.sh for choire

--- problems/choire/data/generator.sh
+++ problems/choire/data/generator.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PPATH=$(realpath ..)
-. ../../../testdata_tools/gen.sh
+. ../../testdata_tools/gen.sh
 
 use_solution herman.py
 
@@ -17,18 +17,24 @@ sample 2
 
 group group1 25
 limits n=1000
+tc 1
+tc 2
 tc small-1 gen_random n=1000 k=1000 maxa=$MAXA mode=random 1
 tc small-2 gen_random n=1000 k=1000 maxa=$MAXA mode=star 2
 tc small-3 gen_random n=1000 k=1000 maxa=$MAXA mode=long 3
 
 group group2 30
 limits n=$MAXN k=10
+tc 1
+tc 2
 tc few-1 gen_random n=$MAXN k=10 maxa=$MAXA mode=random 4
 tc few-2 gen_random n=$MAXN k=10 maxa=$MAXA mode=star 5
 tc few-3 gen_random n=$MAXN k=10 maxa=$MAXA mode=long 6
 
 group group3 45
 limits n=$MAXN
+tc 1
+tc 2
 tc large-1 gen_random n=$MAXN k=$MAXN maxa=$MAXA mode=random 7
 tc large-2 gen_random n=$MAXN k=$MAXN maxa=$MAXA mode=star 8
 tc large-3 gen_random n=$MAXN k=$MAXN maxa=$MAXA mode=long 9
